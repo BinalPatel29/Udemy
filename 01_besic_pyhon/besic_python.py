cc0c1043bfb132fdb5f5acc2a43da5c68989a4ea
BinalPatel29 <161274979+BinalPatel29@users.noreply.github.com>
2025-12-10 04:44:00 +0000

    first commit

---
+++ 01_besic_pyhon/besic_python.py
@@ -0,0 +1,11 @@
+# muttable
+ # different datatypes collection
+lst=[]
+print(type(lst))
+
+# add elements of the list
+name=["binal","bhoomi",1,2,3]
+print(name)
+
+#accessing list element
+fruits=["apple","kiwi","banana","cherry"] 
