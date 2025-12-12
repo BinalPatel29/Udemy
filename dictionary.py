5b351f0c4f53ca685c24bcd43296ba691fc5928b
BinalPatel29 <161274979+BinalPatel29@users.noreply.github.com>
2025-12-12 12:26:11 +0000

    dict

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+#store data in key-value pair
+#unoredered collection of items
+#value can be any types
+#key must be unique and immutable
+
+#creating dictonary
+empty_dict={}
+print(type(empty_dict))
+empty_dict=dic{}
+empty_dict
+
+student={"name":"binal","age":22,"grade":25}
+student
