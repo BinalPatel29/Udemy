794627e30dccd7c707294461c7fcc38c3f856b76
BinalPatel29 <161274979+BinalPatel29@users.noreply.github.com>
2025-12-16 05:43:37 +0000

    updated file

--- 01_besic_pyhon/function/intoduction.py
+++ 01_besic_pyhon/function/intoduction.py
@@ -1,10 +1,10 @@
 #fun is a block of code
 #reusimg code
 #syntax
-def fun_name(parameters):
-    """Docstring"""
-    #fun body
-    return expression
+# def fun_name(parameters):
+#     """Docstring"""
+#     #fun body
+#     return expression
 
 # why fun?
 num=24
@@ -19,22 +19,22 @@ def even_or_odd(num):
     else:
         print("num is odd")
 ##call the fun
-even_or_odd(24)
+# even_or_odd(24)
 
 ##fun with multiple parameters
 def add(a,b):
     return a+b
 result=add(2,4)
-print(result)
+# print(result)
 
 # Default parameters
 def greet(name):
     print(f"Hello {name} welcome to paradise")
-greet("Binal")
+# greet("Binal")
 
 def greet(name="Guest"):             ##  by defualt parameter give "Guest"
     print(f"Hello {name} welcome to paradise")
-greet()
+# greet()
 
 # Variable length arguments
 ##Positional and keyword arguments
@@ -42,26 +42,26 @@ greet()
 def print_numbers(*Binal):    #*args
     for number in Binal:
         print(number) 
-print_numbers(1,2,3,4,5,6,7,8,"Binal")
+# print_numbers(1,3,4,7,5,"Binal")
 
 ### Positional arguments
 def print_numbers(*args):    
     for number in args:
         print(number) 
-print_numbers(1,2,3,4,5,6,7,8,"Binal")
+# print_numbers(1,2,3,4,5,6,7,8,"Binal")
 
 ### Keywords arguments
 def print_details(**kwargs):
     for key,value in kwargs.items():
         print(f"{key}:{value}")
-print_details(name="Binal",age="22",country="india")    
+# print_details(name="Binal",age="22",country="india")    
 
 def print_details(*args,**kwargs):
     for val in args:
         print(f"{val}")
     for key,value in kwargs.items():
         print(f"{key}:{value}")
-print_details(1,2,3,,4,"Binal",name="Binal",age="22",country="india")
+# print_details(1,2,3,4,"Binal",name="Binal",age="22",country="india")
 
 #Return statements
 def multiply(a,b):
