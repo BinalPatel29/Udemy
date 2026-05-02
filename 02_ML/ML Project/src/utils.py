cbabb7380f35974648388d03203e5105f8766611
BinalPatel29 <161274979+BinalPatel29@users.noreply.github.com>
2026-05-02 12:39:34 +0000

    src

--- 02_ML/ML Project/src/utils.py
+++ 02_ML/ML Project/src/utils.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import pickle
+import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 
@@ -40,5 +41,13 @@ def evaluate_models(x_train,y_train,x_test,y_test,models):
 
         return report
     
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
