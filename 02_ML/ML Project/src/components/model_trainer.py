216f4fa989789f7a7963865ba0e89dfdca0fc53d
BinalPatel29 <161274979+BinalPatel29@users.noreply.github.com>
2026-05-02 11:48:20 +0000

    src

--- 02_ML/ML Project/src/components/model_trainer.py
+++ 02_ML/ML Project/src/components/model_trainer.py
@@ -26,7 +26,7 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
 
-    def initiate_model_trainer(self,train_array,test_array,preprocessor_path)
+    def initiate_model_trainer(self,train_array,test_array):
         try:
             logging.info("split training and test input data")
             x_train,y_train,x_test,y_test=(
@@ -46,7 +46,7 @@ class ModelTrainer:
                 "adaboost classifier": AdaBoostRegressor(),
                 }
             
-            model_report:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,model=models)
+            model_report:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models)
 
             best_model_score=max(sorted(model_report.values()))
 
@@ -59,5 +59,15 @@ class ModelTrainer:
                 raise CustomException("no best model found")
             logging.info(f"best found model on both training and testing dataset")
 
-        except:
-            pass
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted=best_model.predict(x_test)
+
+            r2= r2_score(y_test,predicted)
+            return r2
+
+        except Exception as e:
+            raise CustomException(e,sys)
